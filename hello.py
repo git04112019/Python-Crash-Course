744b2806c4ac100bf3eadf9ca16b974d1f3a1f0e
Vinh Hoang <nhuvinh.hoang@gmail.com>
2019-05-14 12:40:06 +0700

    Add more in hello.py

--- hello.py
+++ hello.py
@@ -171,7 +171,69 @@ def getSum(num1, num2):
 print(getSum(10, 20))
 
 
+# Lambda function: similar to JS arrow functions
+
+# getSum= lambda num1, num2: num1 + num2
+
+
+print(getSum(10, 3))
+
+
 # Conditionals
+
+x = 10
+y = 5
+
+# Simple if
+if x > y:
+    print(f'{x} is greater than {y}')
+
+# If/else
+if x > y:
+    print(f'{x} is greater than {y}')
+else:
+    print(f'{y} is greater than {x}')
+
+# elif
+if x > y:
+    print(f'{x} is greater than {y}')
+elif x == y:
+    print(f'{x} is equal to {y}')
+else:
+    print(f'{y} is greater than {x}')
+
+
+# Nested if
+if x > 2:
+    if x <= 10:
+        print(f'{x} is greater than 2 and less than or equal to 10')
+
+
+# and, or, not
+if x > 2 and x <= 10:
+    print(f'{x} is greater than 2 and less than or equal to 10')
+
+if x > 2 or x <= 10:
+    print(f'{x} is greater than 2 and less than or equal to 10')
+
+if not (x == y):
+    print(f'{x} is not equal to {y}')
+
+# Membership operatos: not, not in, is, is not
+
+numbers = [1, 2, 3, 4, 5]
+if x in numbers:
+    print(x in numbers)
+
+if x not in numbers:
+    print(x not in numbers)
+
+if x is y:
+    print(x is y)
+
+if x is not y:
+    print(x is not y)
+
 # Loops
 # Modules
 # Classes & Objects
